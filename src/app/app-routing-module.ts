f113eb1c70033f49e83d47e4e7e6e189c41e8148
nicoledani <nicolle.valverde01@uceva.edu.co>
2026-09-05 00:16:29 -0500

    Fix: Corregir Redireccionamiento A Modulos

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -7,20 +7,13 @@ const routes: Routes = [
     loadChildren: () => import('./modules/pokelist/pokelist-module').then((m) => m.PokelistModule),
   },
   {
-    path: '**',
-    redirectTo: 'pokelist',
     path: 'movielist',
-    loadChildren: () => import('./modules/movielist/movielist-module').then(m => m.MovielistModule)
+    loadChildren: () =>
+      import('./modules/movielist/movielist-module').then((m) => m.MovielistModule),
   },
   {
-    path: '**',
-    redirectTo: 'movielist'
-    path: "libros",
+    path: 'libros',
     loadChildren: () => import('./modules/libros/libros.module').then((m) => m.LibrosModule),
-  },
-  {
-    path: '**',
-    redirectTo: 'libros'
   },
 ];
 
