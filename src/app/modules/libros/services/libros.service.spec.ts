d1b7b8ab7aed9167d18ac5d2357075eabc34f14e
angela-aponte <angela.aponte01@uceva.edu.co>
2026-09-04 23:17:33 -0500

    Test: Se realizan pruebas unitarias

--- src/app/modules/libros/services/libros.service.spec.ts
+++ src/app/modules/libros/services/libros.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LibrosService } from './libros.service';
+import { LIBROS } from '../../../core/config/libros.config';
 
 describe('LibrosService', () => {
   let service: LibrosService;
@@ -13,4 +14,12 @@ describe('LibrosService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('getLibros deberia retornar un observable con los datos de libros', (done) => {
+    service.getLibros().subscribe(libros => {
+      expect(libros).toEqual(LIBROS);
+      expect(libros.length).toBe(LIBROS.length);
+      done();
+    })
+  })
 });
