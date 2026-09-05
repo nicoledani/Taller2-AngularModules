86ca24b7718cf65c6c4372206297222a9cd24397
Nicolle Daniela Valverde Gallego <nicolle.valverde01@uceva.edu.co>
2026-09-04 20:02:37 -0500

    Refactor: Eliminar Modulos Producs Y Users

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,18 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'users',
-    loadChildren: () => import('./modules/users/users-module').then(m => m.UsersModule)
-  },
-  {
-    path: 'products',
-    loadChildren: () => import('./modules/products/products-module').then(m => m.ProductsModule)
-  },
-  {
-    path: '**',
-    redirectTo: 'users'
-  },
+
 ];
 
 @NgModule({
